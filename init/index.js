b5623ad930049353bb5ee556f520f0cefd5867e4
Vishu-15 <vsbangalore15@gmail.com>
2024-12-06 21:18:09 +0530

    initialized listings

--- init/index.js
+++ init/index.js
@@ -2,6 +2,8 @@ const mongoose=require('mongoose');
 const initData=require('./data.js');
 const Listing=require('../models/listing.js');
 
+const dbUrl = process.env.ATLAS_DB_URL;
+
 main()
     .then(()=>{
         console.log('connection succesful');
@@ -9,12 +11,10 @@ main()
     .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(dbUrl);
 };
 
 const initDB=async()=>{
-  await Listing.deleteMany({});
-  initData.data=initData.data.map((obj)=>({...obj,owner:'6633c465198115935e680084'}));
   await Listing.insertMany(initData.data);
   console.log('data was initialized');
 };
